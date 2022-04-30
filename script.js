05ba6ccf125e00b86f17ec5e211363416656738e
victor bobkov <victorbobkov@gmail.com>
2022-04-30 06:19:48 +0400

    Create enemy rectangle & add velocity

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ canvas.height = 576
 context.fillRect(0, 0, canvas.width, canvas.height)
 
 class Sprite {
-   constructor(position) {
+   constructor({ position, velocity }) {
       this.position = position
+      this.velocity = velocity
    }
 
    draw() {
@@ -18,10 +19,35 @@ class Sprite {
 }
 
 const player = new Sprite({
-   x: 0,
-   y: 0
+   position: {
+      x: 0,
+      y: 0
+   },
+   velocity: {
+      x: 0,
+      y: 0
+   }
 })
 
 player.draw()
 
+const enemy = new Sprite({
+   position: {
+      x: 400,
+      y: 100
+   },
+   velocity: {
+      x: 400,
+      y: 100
+   }
+})
+
+enemy.draw()
+
 console.log(player)
+
+const animate = () => {
+   window.requestAnimationFrame(animate)
+}
+
+animate()
