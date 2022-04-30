c099d809dfeae715e90777b2034eb5a425e7f203
victor bobkov <victorbobkov@gmail.com>
2022-04-30 06:42:24 +0400

    Finish creating player & enemy rectangles

--- script.js
+++ script.js
@@ -6,16 +6,31 @@ canvas.height = 576
 
 context.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
+
 class Sprite {
    constructor({ position, velocity }) {
       this.position = position
       this.velocity = velocity
+      this.height = 150
    }
 
    draw() {
       context.fillStyle = 'red'
-      context.fillRect(this.position.x, this.position.y, 50, 150)
+      context.fillRect(this.position.x, this.position.y, 50, this.height)
    }
+
+   update() {
+      this.draw()
+      this.position.y += this.velocity.y
+
+      if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+         this.velocity.y = 0
+      } else {
+         this.velocity.y += gravity
+      }
+   }
+
 }
 
 const player = new Sprite({
@@ -29,25 +44,25 @@ const player = new Sprite({
    }
 })
 
-player.draw()
-
 const enemy = new Sprite({
    position: {
       x: 400,
       y: 100
    },
    velocity: {
-      x: 400,
-      y: 100
+      x: 0,
+      y: 0
    }
 })
-
-enemy.draw()
 
 console.log(player)
 
 const animate = () => {
    window.requestAnimationFrame(animate)
+   context.fillStyle = 'black'
+   context.fillRect(0, 0, canvas.width, canvas.height)
+   player.update()
+   enemy.update()
 }
 
 animate()
