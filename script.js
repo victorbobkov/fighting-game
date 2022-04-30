ba7e1aea4875b2479194e8c6895b9541c12de954
victor bobkov <victorbobkov@gmail.com>
2022-04-30 06:10:23 +0400

    Create player rectangle

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ canvas.width = 1024
 canvas.height = 576
 
 context.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+   constructor(position) {
+      this.position = position
+   }
+
+   draw() {
+      context.fillStyle = 'red'
+      context.fillRect(this.position.x, this.position.y, 50, 150)
+   }
+}
+
+const player = new Sprite({
+   x: 0,
+   y: 0
+})
+
+player.draw()
+
+console.log(player)
