1483dada76f732a6aea62321cc43f725fc3ca5ab
victor bobkov <victorbobkov@gmail.com>
2022-05-05 05:32:19 +0400

    Fix player & enemy position

--- script.js
+++ script.js
@@ -67,43 +67,6 @@ const keys = {
    }
 }
 
-const rectangularCollision = ({ rectangle1, rectangle2 }) => {
-   return (
-      rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-      rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-      rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-      rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-   )
-}
-
-const determineWinner = ({ player, enemy, timerId }) => {
-   clearTimeout(timerId)
-
-   document.querySelector('#result-text').style.display = 'flex'
-
-   if (player.health === enemy.health) {
-      document.querySelector('#result-text').innerHTML = `Tie!`
-   } else if (player.health > enemy.health) {
-      document.querySelector('#result-text').innerHTML = `Player 1 Wins!`
-   } else if (player.health < enemy.health) {
-      document.querySelector('#result-text').innerHTML = `Player 2 Wins!`
-   }
-}
-
-let timer = 5
-let timerId
-const decreaseTimer = () => {
-   if (timer > 0) {
-      timerId = setTimeout(decreaseTimer, 1000)
-      timer--
-      document.querySelector('#timer').innerHTML = timer
-   }
-
-   if (timer === 0) {
-      determineWinner({ player, enemy, timerId })
-   }
-}
-
 decreaseTimer()
 
 const animate = () => {
